ec76dea45b058f949338abf68abcffdc0d3447db
Amir <114600063+amirjonu@users.noreply.github.com>
2024-01-25 01:17:01 +0000

    helo

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,9 @@
 import React from "react";
-import './Home.css'; 
+import "./Home.css";
 import { motion } from "framer-motion";
 
-
-
-
 function Home() {
-  const text = "Welcome to DWJ Curbside".split(" ");
+  const text = "DWJ Curbside".split(" ");
   return (
     <div className="home">
       {text.map((el, i) => (
@@ -15,17 +12,14 @@ function Home() {
           animate={{ opacity: 1 }}
           transition={{
             duration: 3,
-            delay: i / 2.5
+            delay: i / 2.5,
           }}
           key={i}
         >
           {el}{" "}
         </motion.span>
       ))}
-      
-
     </div>
-  
   );
 }
 
