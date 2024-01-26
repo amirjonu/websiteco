4c28720a67a1c93492d4fe0868495919ab64c6f8
Amirjon Ulmasov <amirjonu@nycstudents.net>
2024-01-26 11:44:28 -0500

    please work

--- src/App.js
+++ src/App.js
@@ -1,61 +1,7 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-
-import Navbar from "./components/Navbars";
 import "./App.css";
-import Home from "./routes/Home";
-import About from "./routes/About";
-import Contact from "./routes/Contact";
-import Purchase from "./routes/Purchase";
-import Appointment from "./routes/Appoitment";
-import ErrorPage from "./routes/ErrorPage";
-import Footer from "./components/Footer";
 
-// https://github.com/briancodex/react-navbar-router-v6.4
-// Navbar and routing is from above link ^^^^
+function App() {
+  return <div className="App"></div>;
+}
 
-export default function App(){
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </>
-  );
-};
-
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "appoitment",
-        element: <Appointment />,
-      },
-
-      {
-        path: "purchase",
-        element: <Purchase />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-    ],
-  },
-]);
-
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />,
-);
+export default App;
